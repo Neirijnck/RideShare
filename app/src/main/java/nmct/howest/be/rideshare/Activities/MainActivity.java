e21d177633069e99804f397ca62f0826e105ba94
Neirijnck <neirijnck@hotmail.com>
2014-10-22 10:11:02 +0200

    Design - V1.1 - Holo Light with dark action bar

--- app/src/main/java/nmct/howest/be/rideshare/Activities/MainActivity.java
+++ app/src/main/java/nmct/howest/be/rideshare/Activities/MainActivity.java
@@ -1,9 +1,11 @@
-package nmct.howest.be.rideshare;
+package nmct.howest.be.rideshare.Activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import nmct.howest.be.rideshare.R;
 
 
 public class MainActivity extends ActionBarActivity {
