495824b18cd8eaf6f98d109c995cf7853fa6ffc4
Neirijnck <neirijnck@hotmail.com>
2014-10-29 17:26:49 +0100

    Design - V2.2 - Volledig thema opnieuw gemaakt + nieuw icon

--- app/src/main/java/nmct/howest/be/rideshare/Activities/Fragments/PlannenFragment.java
+++ app/src/main/java/nmct/howest/be/rideshare/Activities/Fragments/PlannenFragment.java
@@ -3,7 +3,6 @@ package nmct.howest.be.rideshare.Activities.Fragments;
 /**
  * Created by Preben on 27/10/2014.
  */
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,10 +11,24 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
 import nmct.howest.be.rideshare.R;
 
 public class PlannenFragment extends Fragment
 {
+    //Variables
+    private Switch herhaalSwitch;
+    private ToggleButton tglMaandag;
+    private ToggleButton tglDinsdag;
+    private ToggleButton tglWoensdag;
+    private ToggleButton tglDonderdag;
+    private ToggleButton tglVrijdag;
+    private ToggleButton tglZaterdag;
+    private ToggleButton tglZondag;
+
     public PlannenFragment() {}
 
     @Override
@@ -27,7 +40,49 @@ public class PlannenFragment extends Fragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_plannen, container, false);
+        View view = inflater.inflate(R.layout.fragment_plannen, container, false);
+
+        //Get widgets
+        herhaalSwitch = (Switch) view.findViewById(R.id.switchHerhaal);
+        tglMaandag = (ToggleButton) view.findViewById(R.id.tglMaandag);
+        tglDinsdag = (ToggleButton) view.findViewById(R.id.tglDinsdag);
+        tglWoensdag = (ToggleButton) view.findViewById(R.id.tglWoensdag);
+        tglDonderdag = (ToggleButton) view.findViewById(R.id.tglDonderdag);
+        tglVrijdag = (ToggleButton) view.findViewById(R.id.tglVrijdag);
+        tglZaterdag = (ToggleButton) view.findViewById(R.id.tglZaterdag);
+        tglZondag = (ToggleButton) view.findViewById(R.id.tglZondag);
+
+
+        //Enable togglebuttons when switch is on
+        herhaalSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked)
+                {
+                    //Enable all weektogglebuttons
+                    tglMaandag.setEnabled(true);
+                    tglDinsdag.setEnabled(true);
+                    tglWoensdag.setEnabled(true);
+                    tglDonderdag.setEnabled(true);
+                    tglVrijdag.setEnabled(true);
+                    tglZaterdag.setEnabled(true);
+                    tglZondag.setEnabled(true);
+                }
+                else
+                {
+                    //Disable all weektogglebuttons
+                    tglMaandag.setEnabled(false);
+                    tglDinsdag.setEnabled(false);
+                    tglWoensdag.setEnabled(false);
+                    tglDonderdag.setEnabled(false);
+                    tglVrijdag.setEnabled(false);
+                    tglZaterdag.setEnabled(false);
+                    tglZondag.setEnabled(false);
+                }
+            }
+        });
+
+        return view;
     }
 
     @Override
--- app/src/main/java/nmct/howest/be/rideshare/Activities/MainActivity.java
+++ app/src/main/java/nmct/howest/be/rideshare/Activities/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.res.Configuration;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
 import nmct.howest.be.rideshare.Activities.Adapters.TabPagerAdapter;
 import nmct.howest.be.rideshare.R;
 
@@ -67,6 +65,7 @@ public class MainActivity extends FragmentActivity {
     }
 
 
+    //Menu is per fragment
 /*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
