bfae337ee61902382438f5f07dbb001ddb0fedcb
Neirijnck <neirijnck@hotmail.com>
2014-11-05 12:19:59 +0100

    Design - V2.6 - Login Activity; animations

--- app/src/main/java/nmct/howest/be/rideshare/Activities/Fragments/LearnMoreFragment.java
+++ app/src/main/java/nmct/howest/be/rideshare/Activities/Fragments/LearnMoreFragment.java
@@ -1,12 +1,14 @@
 package nmct.howest.be.rideshare.Activities.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import nmct.howest.be.rideshare.R;
 
 /**
@@ -14,10 +16,48 @@ import nmct.howest.be.rideshare.R;
  */
 public class LearnMoreFragment extends Fragment
 {
+
+    private FragmentActivity context;
+    private TextView getBack;
+    private FragmentTransaction transaction;
+    private LoginFragment loginFragment;
+
     public LearnMoreFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_learnmore, container, false);
+        View view = inflater.inflate(R.layout.fragment_learnmore, container, false);
+
+        getBack = (TextView) view.findViewById(R.id.txbGetBack);
+        //Set click listener for "Learn more" textview
+        getBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginFragment = new LoginFragment();
+                slideDownFragment();
+            }
+        });
+
+        return view;
     }
+
+    @Override
+    public void onAttach(Activity activity) {
+        context=(FragmentActivity) activity;
+        super.onAttach(activity);
+    }
+
+    private void slideDownFragment()
+    {
+        transaction = context.getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment
+        transaction.setCustomAnimations(R.anim.enter_from_top, R.anim.exit_out_bottom);
+        transaction.replace(R.id.fragment_container, loginFragment);
+
+        // Commit the transaction
+        transaction.commit();
+    }
+
 }
